0c789eefebed305adceb74a06a47d52a25ea86e0
Vinicius Dias <viniragh@gmail.com>
2025-04-08 22:53:34 -0300

    criando login componente

---
+++ Front-End/call-service-ui/src/app/pages/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  username = '';
+  password = '';
+
+  login() {
+    console.log('Login com:', this.username, this.password);
+    // Aqui você pode chamar um service de autenticação
+  }
+}
